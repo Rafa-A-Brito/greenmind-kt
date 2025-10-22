6eded3e3b7ecae2baf060950382e12de1ab094b9
Ꝃrꝋnꝋscoder <rafabrito1903@gmail.com>
2025-10-22 12:15:23 +0000

    Alteração base de dados 🏦

--- app/src/main/java/com/github/rafaabrito/projectgreenmind/data/db/GreenMindDatabase.kt
+++ app/src/main/java/com/github/rafaabrito/projectgreenmind/data/db/GreenMindDatabase.kt
@@ -3,10 +3,25 @@ package com.github.rafaabrito.projectgreenmind.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.github.rafaabrito.projectgreenmind.data.db.dao.UserDao
+import android.content.Context
 
 @Database(entities = [UserEntity::class], version = 1)
 abstract class GreenMindDatabase: RoomDatabase(){
 
     abstract fun userDao(): UserDao
-    //
+    
+    companion object {
+        @Volatile private var INSTANCE: GreenMindDatabase?=null
+
+        fun getDatabase(context: Context):GreenMindDatabase{
+            return INSTANCE?: synchronized(this){
+                Room.databaseBuilder(
+                    content.ApplicationContext,
+                    GreenMindDatabase::class.java,
+                    "greenmind_db
+                ).build
+                .also{ INSTANCE = it}
+            }
+        }
+    }
 }
--- app/src/main/java/com/github/rafaabrito/projectgreenmind/data/db/UserEntity.kt
+++ app/src/main/java/com/github/rafaabrito/projectgreenmind/data/db/UserEntity.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "user" )
 data class UserEntity (
-    @PrimaryKey val  id: Int = 0,
+    @PrimaryKey(autogenerate = true) val  id: Int = 0,
     val name: String,
     val email: String,
     val password: String,
