a2a14cc74425cae17929fec1ec8da63b1069d230
Ꝃrꝋnꝋscoder <rafabrito1903@gmail.com>
2025-10-24 11:32:08 -0300

    Column Info para Users

--- app/src/main/java/com/github/rafaabrito/projectgreenmind/data/db/UserEntity.kt
+++ app/src/main/java/com/github/rafaabrito/projectgreenmind/data/db/UserEntity.kt
@@ -1,12 +1,13 @@
 package com.github.rafaabrito.projectgreenmind.data.db
 
 import androidx.room.Entity
+import androidx.room.ColumnInfo
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "user" )
 data class UserEntity (
-    @PrimaryKey(autogenerate = true) val  id: Int = 0,
-    val name: String,
-    val email: String,
-    val password: String,
+    @PrimaryKey(autogenerate = true) val  id: Int?,
+    @ColumnInfo(name = "name") val name: String?,
+    @ColumnInfo(name = "email") val email: String?,
+    @ColumnInfo(name = "password") val password: String?,
 )
