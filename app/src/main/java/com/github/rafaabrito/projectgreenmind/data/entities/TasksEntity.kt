a3c86bf6d67c511bbc6311e41f1296eb5e16c530
Ꝃrꝋnꝋscoder <rafabrito1903@gmail.com>
2025-10-27 10:17:50 -0300

    Adição colunas de Tarefas 🏡

--- app/src/main/java/com/github/rafaabrito/projectgreenmind/data/entities/TasksEntity.kt
+++ app/src/main/java/com/github/rafaabrito/projectgreenmind/data/entities/TasksEntity.kt
@@ -1,10 +1,16 @@
 package com.github.rafaabrito.projectgreenmind.data.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "tasks")
 data class TasksEntity(
     @PrimaryKey(autoGenerate = true) val  id: Int = 0,
+    @ColumnInfo(name="description") val description: String,
+    @ColumnInfo(name="missionTitle") val missionTitle: String,
+    @ColumnInfo(name="finalPercentual") val finalPercentual: Float,
+    @ColumnInfo(name="missionType") val missionType: String,
+    @ColumnInfo(name="rewardsPoints") val rewardPoints: Int,
 
     )
