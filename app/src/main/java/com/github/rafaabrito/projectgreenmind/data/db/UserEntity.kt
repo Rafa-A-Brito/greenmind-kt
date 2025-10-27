a98842150a268d963cfdcffb262e68e2508ee5d6
Rafa-A-Brito <rafabrito1903@gmail.com>
2025-10-27 00:00:24 -0300

    UserEntity column

---
+++ app/src/main/java/com/github/rafaabrito/projectgreenmind/data/db/UserEntity.kt
@@ -0,0 +1,13 @@
+package com.github.rafaabrito.projectgreenmind.data.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user" )
+data class UserEntity (
+    @PrimaryKey(autoGenerate = true) val  id: Int = 0,
+    @ColumnInfo(name="name") val name: String?,
+    @ColumnInfo(name="email") val email: String?,
+    @ColumnInfo(name="password") val password: String?,
+)
